6c6d1e1103496889dd1fb907438776f9699abd97
YeonGyu Kim <public.kim.yeon.gyu@gmail.com>
2020-04-20 12:33:13 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# EBS Lecture auto-listener
+
+개인적인 호기심으로 제작하였으나 사용을 권장하지는 않습니다.
+javscript 소스를 사용해 완강처리를 시켜버리는것이 아닌, 크롬을 백그라운드로 돌려 강의를 듣는것과 100% 똑같이 처리하는 방법입니다.
+
+## 준비물
+본 프로젝트는 selenium과 파이썬을 사용합니다.  
+프로젝트를 다운로드 받은 뒤, <https://chromedriver.chromium.org/downloads> 에서 현재 사용중인 크롬과 같은 버전의 드라이버를 다운받아주세요. 이 방법으로 크롬드라이버를 설치 해주세요.
+
+<https://chromedriver.chromium.org/downloads> 에서 다운로드 받은 파일의 압축을 해제하고, "chromedriver.exe" 혹은 "chromedriver" 라는 이름으로 된 파일을 lecture.py가 위치한곳과 같은 경로에 넣어주세요.
+
+파이썬은 검색을 통해 설치해주시면 좋겠습니다.
+파이썬을 설치 한 이후,
+```bash
+pip install -r requirements.txt
+```
+를 통해 selenium 모듈을 설치해주세요.
+
+## 설정
+lecture_list.txt에 수강처리하고싶은 강의를 넣어주세요.  
+본 프로젝트는 자동으로 강의 동영상을 순차적으로 재생시키는 원리를 갖고있습니다.  
+따라서, lecture_list.txt에 들어가는 링크는 동영상이 포함된 강의의 링크여야합니다.
+
+## 사용
+본 파이썬 파일을 실행해주시면 됩니다. *nix 게열의 운영체제에서는:
+```bash
+python3 lecture.py
+```
+NT 계열의 운영체제에서는:
+```powershell
+python lecture.py
+```
+
+## 주의
+소스가 궁금하다거나, 어떻게 작동한다거나 하는 호기심을 제외하고는, 본 코드를 사용하지 않는것을 매우 강력하게 권장합니다.  
+이 코드를 사용함으로써 생길 수 있는 모든 문제는 사용자의 책임입니다.
---
+++ lecture.py
@@ -0,0 +1,71 @@
+import random
+import datetime
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.alert import Alert
+
+chrome_options = Options()
+chrome_options.add_argument("--mute-audio")
+try:
+    try:
+        driver = webdriver.Chrome("./chromedriver",
+                                  chrome_options=chrome_options)
+    except:
+        driver = webdriver.Chrome("./chromedriver.exe",
+                                  chrome_options=chrome_options)
+except:
+    print("chromedriver not found")
+    exit(-1)
+
+# 로그인 부분
+driver.get("https://hoc22.ebssw.kr/sso/loginView.do")
+
+input("로그인 하고 엔터를 눌러주세요.")
+
+links = [
+    link.replace("\n", '')
+    for link in open('lecture_list.txt', 'r').readlines()
+]
+for link in links:
+    print("lecture_list.txt 안의 강의들을 수강합니다. 다음과 같은 링크들이 입력되었습니다: ")
+    print(link)
+
+print("\n\n--------------------\n수강 시작합니다. 한번에 하나의 강의만 수강합니다.")
+
+for link in links:
+    total_time = 0
+    print(f"{link} 수강 시작")
+    driver.get(link)
+    while True:
+        driver.refresh()
+        sleep(2)
+        time_str = driver.execute_script(
+            'return document.querySelector("#playerEl > div.vjs-control-bar > div.vjs-duration.vjs-time-control.vjs-control > span.vjs-duration-display").innerText'
+        )
+        if time_str != "0:00":
+            break
+    print(f"{datetime.datetime.now().time()}: 강의 수강 시작, 강의 길이: {time_str}")
+    time_list = time_str.split(":")
+    total_time += int(time_list[-1])
+    total_time += int(time_list[-2]) * 60
+    print("동영상 재생")
+    try:
+        driver.find_element_by_css_selector('#playerEl > button').click()
+    except:
+        driver.find_element_by_css_selector(
+            '#playerEl > div.vjs-message-display.vjs-modal-dialog > div > div > div.vjs-modal-dialog-buttonWrap > button.vjs-modal-dialog-btn_ok'
+        ).click()
+    if len(time_list) == 3:
+        total_time += int(time_list[0]) * 3600
+    sleep(total_time)
+    while current_time != time_str:
+        current_time = driver.execute_script(
+            'return document.querySelector("#playerEl > div.vjs-control-bar > div.vjs-current-time.vjs-time-control.vjs-control > span.vjs-current-time-display").innerText'
+        )
+        sleep(2)
+    driver.execute_script(
+        'document.querySelector("#learn_header > div > ul > li > a").click()')
+    sleep(2)
+print("모든 강의의 수강이 완료 되었습니다.")
+driver.close()
